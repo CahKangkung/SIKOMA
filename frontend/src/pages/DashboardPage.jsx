7a8a4d11f60ad2ed81c277f0487d225d01c1e891
liyanti <eliyan.liyanti@gmail.com>
2025-10-28 13:47:12 +0700

    Header.jsx

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,5 +1,5 @@
 // src/pages/DashboardPage.jsx
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/SideBar";
 import Header from "../components/Header";
 
 export default function DashboardPage() {
