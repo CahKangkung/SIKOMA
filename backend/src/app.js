fbd7607c71ce31af8c6f9e1d12a76e588cbf0b68
liyanti <eliyan.liyanti@gmail.com>
2025-11-03 15:07:32 +0700

    aaaa

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,25 +4,39 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"; 
 import mongoose from 'mongoose';
+import { MongoClient } from "mongodb";
+
 dotenv.config();
 
+// routes
 import ingestRoutes from "./routes/ingest.routes.js";
 import searchRoutes from "./routes/search.routes.js";
 import docsRoutes from "./routes/docs.routes.js";
 import filesRoutes from "./routes/files.routes.js";
 import userRoutes from "./routes/userRoutes.js";
+import db from "./models/User.js";
 
+// app init 
 const app = express();
-app.use(cors());
+
+// middlewares
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 app.use(express.json({ limit: "25mb" }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 
+// routes
 app.use("/api", ingestRoutes);
 app.use("/api", searchRoutes);
 app.use("/api", docsRoutes);
 app.use("/api", filesRoutes);
 
+// error handler 
 app.use((err, _req, res, _next) => {
     console.error(err);
     res.status(500).json({ error: "internal error" });
@@ -61,4 +75,3 @@ app.use("/api/auth", userRoutes);
 app.listen(process.env.PORT, () => {
     console.log(`Server running on :${process.env.PORT}`);
 });
-
